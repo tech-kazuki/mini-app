ea84aa9df264aa2c495bc5ed93c016ae611e4c9a
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-03-31 15:11:35 +0900

    Add contentForm

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,19 +1,17 @@
 class ContentsController < ApplicationController
 
   def index
-    @content = Content.new
   end
 
   def new
   end
 
   def create
-    binding.pry
-    Contents.create(content: content_params[:content], image: content_params[:image], title: content_params[:title], user_id: current_user.id)
+    Content.create(content: content_params[:content], image: content_params[:image], title: content_params[:title], user_id: current_user.id)
   end
 
   private
   def content_params
-    params.parmit(:content, :image, :title)
+    params.require(:content).permit(:content, :image, :title)
   end
 end
