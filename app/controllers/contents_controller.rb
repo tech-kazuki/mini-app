c3a87051011067271696072628a9e3be8a83c3ed
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-02 14:36:55 +0900

    Edit files

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,6 +1,9 @@
 class ContentsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
+    @contents = Content.includes(:user).all
   end
 
   def new
@@ -10,8 +13,24 @@ class ContentsController < ApplicationController
     Content.create(content: content_params[:content], image: content_params[:image], title: content_params[:title], user_id: current_user.id)
   end
 
+  def edit
+    @content = Content.find(params[:id])
+  end
+
+  def update
+    content = Content.find(params[:id])
+    if content.user_id == current_user.id
+      content.update(content_params)
+    end
+    redirect_to action: :index
+  end
+
   private
   def content_params
     params.require(:content).permit(:content, :image, :title)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
