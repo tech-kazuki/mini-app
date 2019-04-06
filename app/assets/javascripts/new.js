2e2b6fefa6cc5b4bd7b7cac71b7d228400c94875
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-06 12:29:24 +0900

    Edit file

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -3,7 +3,7 @@ $(document).on('turbolinks:load', function () {
     // 現在日時
     var current = new Date();
     // 14日（2週間）前のミリ秒
-    var range_ms = current.getTime() - (5 * 24 * 60 * 60 * 1000);
+    var range_ms = current.getTime() - (1 * 24 * 60 * 60 * 1000);
 
     // 更新日時
     var modified = new Date($(this).attr('datetime'));
